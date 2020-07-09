09f333a66df73051c84f2951ba11fab7939b148a
Anwesh43 <anweshthecool0@gmail.com>
2020-07-09 14:49:28 +0530

    adding constants and libraries

--- vertbisidebarview/src/main/java/com/anwesh/uiprojects/vertbisidebarview/VertBiSideBarView.kt
+++ vertbisidebarview/src/main/java/com/anwesh/uiprojects/vertbisidebarview/VertBiSideBarView.kt
@@ -3,3 +3,19 @@ package com.anwesh.uiprojects.vertbisidebarview
 /**
  * Created by anweshmishra on 09/07/20.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+import android.content.Context
+import android.app.Activity
+
+val colors : Array<String> = arrayOf("#F44336", "#03A9F4", "#3F51B5", "#4CAF50", "#009688")
+val bars : Int = 2
+val scGap : Float = 0.02f / (bars + 2)
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 20
+val sizeFactor : Float = 4f
