54b9255bb7fd79d77413eb35ae034398d46c1707
Anwesh43 <anweshthecool0@gmail.com>
2020-07-09 15:20:12 +0530

    adding VBSBNode class

--- vertbisidebarview/src/main/java/com/anwesh/uiprojects/vertbisidebarview/VertBiSideBarView.kt
+++ vertbisidebarview/src/main/java/com/anwesh/uiprojects/vertbisidebarview/VertBiSideBarView.kt
@@ -116,4 +116,45 @@ class VertBiSideBarView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class VBSBNode(var i : Int, val state : State = State()) {
+
+        private var next : VBSBNode? = null
+        private var prev : VBSBNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = VBSBNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawVBSBNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : VBSBNode {
+            var curr : VBSBNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
