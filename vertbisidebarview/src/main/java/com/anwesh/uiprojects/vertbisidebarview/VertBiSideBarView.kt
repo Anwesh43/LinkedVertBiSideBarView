ad98358c892232f30bf90aedf79e7f648ad57cda
Anwesh43 <anweshthecool0@gmail.com>
2020-07-09 15:03:23 +0530

    adding Canvas extension method

--- vertbisidebarview/src/main/java/com/anwesh/uiprojects/vertbisidebarview/VertBiSideBarView.kt
+++ vertbisidebarview/src/main/java/com/anwesh/uiprojects/vertbisidebarview/VertBiSideBarView.kt
@@ -24,3 +24,32 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawVertBiSideBar(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, 2)
+    val sf2 : Float = sf.divideScale(1, 2)
+    save()
+    translate(w / 2, h / 2)
+    save()
+    translate(h / 2, 0f)
+    drawRect(RectF(-size / 2, -h * sf1, size / 2, 0f), paint)
+    restore()
+    for (j in 0..1) {
+        val sf2i : Float = sf2.divideScale(j, 2)
+        save()
+        scale(1f - 2 * j, 1f - 2 * j)
+        translate(0f, h / 2)
+        drawRect(RectF(-(w / 2 * sf2i), -size, 0f, 0f), paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawVBSBNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    drawVertBiSideBar(scale, w, h, paint)
+}
